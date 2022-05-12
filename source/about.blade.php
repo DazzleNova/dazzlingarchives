a1a3f0a2ef682d4b7bd57c3b3c4579ab11f3e7d3
Star <DazzyDillsROCKS@gmail.com>
2022-05-12 05:32:52 -0400

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -3,14 +3,15 @@
 @section('title', 'About')
 
 @section('content')
-    <h1>About</h1>
+    <h1>About the Dazzling Archives</h1>
 
-    <p>My name is {{ $page->owner->name }}</p>
+    <p>My name is {{ $page->owner->name }}, and this is a personal space to archive all of my small things. 
+    The things that will be archive here are my original art, written work that wasn't posted on my main website, and "edits" that I made thoughout the years.</p>
 
-    <h2>Links:</h2>
+    <h2>My links</h2>
 
     <ul>
-        <li><a href="/twitter" target="_blank">Twitter</a></li>
-        <li><a href="/github" target="_blank">GitHub</a></li>
+        <li><a href="https://dazzlinggleam.space/" target="_blank">My Website</a></li>
+        <li><a href="https://misskey.io/@dazzle" target="_blank">My Misskey account</a></li>
     </ul>
 @endsection
