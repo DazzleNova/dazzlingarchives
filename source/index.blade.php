591dfe371734e89559860b99226ae138d4769f95
Star <DazzyDillsROCKS@gmail.com>
2022-05-12 05:51:36 -0400

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -6,5 +6,6 @@
     <p>Here's is the archives that weren't uploaded to my main website because I don't want to increase the total size of my website and don't want to bloat it.</p>
 
     <p>Over time, this place will be filled with things I created throughout the years. I wanted to create a single place to store everything instead of hosting them everywhere to the point it's hard to find thing myself. This will be more convinient for me.</p>
-
+    <br>
+    <p><a href="https://dazzlinggleam.space/" target="_blank">Back to main website</a></p>
 @endsection
